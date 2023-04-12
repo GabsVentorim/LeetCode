73b4912ee0fda659a87e3d9aeb95a25ef69e019a
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-04-12 20:01:45 -0300

    add 200 java

--- 200. Number of Islands.java
+++ 200. Number of Islands.java
@@ -1,5 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j ++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    solve(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
 
+    public void solve(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
+            return; 
+        grid[i][j] = '0';
+        solve(grid, i + 1, j); // up
+        solve(grid, i - 1, j); // down
+        solve(grid, i, j + 1); // right
+        solve(grid, i, j - 1); // left
     }
 }
